9a712e6d0aec3c8b857186d1bb6b15306c93a92c
Alex Rubiano <alexrubianoromero@gmail.com>
2020-05-04 17:04:39 -0500

    hasta la leccion de uso de hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+ import { AppRoutingModule } from './app-routing.module';
+//import { routing, appRoutingProviders } from './app.routing';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // appRoutingProviders
+    
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
