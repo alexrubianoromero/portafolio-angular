e42a7f9d83debef2715919eaa9073fa202cfdbac
Alex Rubiano <alexrubianoromero@gmail.com>
2020-05-26 11:36:17 -0500

    hasta lca creacion de la promesa

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { promise } from 'protractor';
 
 
 @Injectable({
@@ -9,15 +10,39 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 cargando= true;
 productos : Producto[] = [];
+productosFiltrado : Producto[] = [];
   constructor( private http: HttpClient ) { 
     this.cargarProductos();
   }
   private cargarProductos(){
-      this.http.get('https://angular-html-ac3e2.firebaseio.com/productos_idx.json')
-      .subscribe((resp:Producto[])=>{
-        console.log(resp);
-        this.productos = resp;
-        this.cargando= false;
+
+      return new Promise( (resolve , reject)=>{
+        this.http.get('https://angular-html-ac3e2.firebaseio.com/productos_idx.json')
+        .subscribe((resp:Producto[])=>{
+          //console.log(resp);
+          this.productos = resp;
+          //setTimeout(() => {
+          this.cargando = false;
+          //}, 2000);
+          resolve();
+        });
       });
+
+     
+  }
+
+  getProducto(id:string){
+    return this.http.get(`https://angular-html-ac3e2.firebaseio.com/productos/${ id }.json`);
+  }
+  buscarProducto( termino:string){
+     if( this.productos.length===0){
+       //cargar producots 
+     } else{
+       //aplicxar el filtro
+     }
+    this.productosFiltrado=this.productos.filter(producto=>{
+      return true;
+    });
+    console.log(this.productosFiltrado);
   }
 }
